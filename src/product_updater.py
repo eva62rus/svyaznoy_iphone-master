8be52f3e0eafd37405e9505dd96acf300d4263d2
виктор еремеев <a62wap@mail.ru>
2023-04-25 15:17:40 +0300

    25.04.23

--- src/product_updater.py
+++ src/product_updater.py
@@ -37,6 +37,10 @@ def get_page_count(driver):
         print('Failed to determine the number of pages.')
 
 
+def get_url_for_page(page_num):
+    return SRC_URL + '/page-' + str(page_num)
+
+
 def get_products_info(driver):
     products_info = driver.find_elements(By.XPATH, ElemXpath.product_info.value)
     return [product_info.text for product_info in products_info]
@@ -85,14 +89,23 @@ def parsing_products_info(products_info, products_price):
 
 
 def main():
-    driver = init_driver(headless=True)
+    driver = init_driver(headless=False)
     driver.get(SRC_URL)
     page_count = get_page_count(driver)
-    products_info = get_products_info(driver)
-    products_price = get_products_price(driver)
-    products = parsing_products_info(products_info, products_price)
-    for product in products:
-        print(f'Name: {product.name}, Memory: {product.memory}, Color: {product.color}, Price: {product.price}\n')
+    urls = [get_url_for_page(i) for i in range(1, page_count + 1)]
+    products = []
+    for url in urls:
+        if url != SRC_URL:
+            driver.get(url)
+            sleep(2)
+        products_info = get_products_info(driver)
+        products_price = get_products_price(driver)
+        products += parsing_products_info(products_info, products_price)
+    print(len(products))
+    with open('products.txt', 'a') as f:
+        for product in products:
+            f.write(f'Name: {product.name}, Price: {product.price}, Color: {product.color}, Mem: {product.memory}\n')
+
 
 if __name__ == '__main__':
     main()
